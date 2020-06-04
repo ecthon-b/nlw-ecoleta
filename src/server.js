68d23af53d043872f94d489214ae9c673ba203c4
Ecthon Borhis <ecthon@gmail.com>
2020-06-04 18:41:13 -0400

    Dia 4 - Back-end, nodemon, express, nunjucks

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const server = express()
+
+// configurar pasta pública
+server.use(express.static("public"))
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+
+// configuara caminhos da minha aplicação
+// página inicial
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
+// ligar o servidor
+// ouvir a porta 3000
+server.listen(3000)
+
+
